ea3c93719aa496ac9b516714d52defe623dfa441
1E10 Laptop <sample@bths.edu>
2022-09-30 10:56:58 -0400

    .

---
+++ src/Point.java
@@ -0,0 +1,69 @@
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(int oneNumber) {
+        x = oneNumber;
+        y = oneNumber;
+    }
+
+    public Point() {
+        x = 0;
+        y= 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+
+    public String coordinate() {
+        String coordinate = ("(" + x + ", " + y + ")");
+        return coordinate;
+    }
+
+    public String quadrant() {
+        String quadrant;
+        if (x == 0 && y == 0) {
+            quadrant = "origin";
+        } else {
+            quadrant = "on an axis";
+        }
+
+        if (x > 0 && y > 0) {
+            quadrant = "I";
+        }
+
+        if (x > 0 && y < 0) {
+            quadrant = "IV";
+        }
+
+        if (x < 0 && y > 0) {
+            quadrant = "II";
+        }
+
+        if (x < 0 && y < 0) {
+            quadrant = "III";
+        }
+        return quadrant;
+    }
+    }
+//turtle
---
+++ src/RectangleRunner.java
@@ -0,0 +1,45 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+        /*
+        Rectangle rect1 = new Rectangle(150,200);
+        rect1.setWidth(75);
+        int length1 = rect1.getLength();
+        int width1 = rect1.getWidth();
+        int area1 = rect1.calculateArea();
+        System.out.println("Plot 1 Length: " + length1);
+        System.out.println("Plot 1 Width: " + width1);
+        System.out.println("Plot 1 Area: " + area1);
+        System.out.println();
+
+        Rectangle rect2 = new Rectangle(125);
+        rect2.setWidth(75);
+        rect2.setLength(75);
+        int length2 = rect2.getLength();
+        int width2 = rect2.getWidth();
+        int area2 = rect2.calculateArea();
+        System.out.println("Plot 2 Length: " + length2);
+        System.out.println("Plot 2 Width: " + width2);
+        System.out.println("Plot 2 Area: " + area2);
+        System.out.println();
+
+        Rectangle rect3 = new Rectangle();
+        rect3.setWidth(75);
+        int length3 = rect3.getLength();
+        int width3 = rect3.getWidth();
+        int area3 = rect3.calculateArea();
+        System.out.println("Plot 3 Length: " + length3);
+        System.out.println("Plot 3 Width: " + width3);
+        System.out.println("Plot 3 Area: " + area3);
+        System.out.println();
+
+        int totalArea = area1 + area2 + area3;
+        System.out.println("These three plots require " + totalArea + " square feet of seed.");
+        */
+
+
+
+;
+
+    }
+}
